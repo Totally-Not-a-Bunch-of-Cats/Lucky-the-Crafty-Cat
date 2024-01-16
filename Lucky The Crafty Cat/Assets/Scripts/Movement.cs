14aafaf72f96d1723894eee61bf5515be14c067d
Declin-Anderson <Declin-Anderson@users.noreply.github.com>
2024-01-16 03:40:24 -0600

    Update Movement.cs

--- Lucky The Crafty Cat/Assets/Scripts/Movement.cs
+++ Lucky The Crafty Cat/Assets/Scripts/Movement.cs
@@ -10,12 +10,6 @@ public class Movement : MonoBehaviour
     // The height that the player can jump
     [SerializeField] float jumpHeight = 2f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
